5183dec113afa9e01bf16455b940076cc3dd268f
Terry Gilmore <tgilly93@gmail.com>
2025-03-22 11:40:44 -0400

    TG settuing up Portfolio. Creating NavBar component.

---
+++ portfolio_app/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import NavBar from './components/NavBar';
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <NavBar />
+      <h1>Welcome to my Portfolio!</h1>
+    </>
+  )
+}
+
+export default App;
