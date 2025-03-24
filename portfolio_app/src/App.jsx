901a08e44928dc087f756a2bdbf910100d5bae54
Terry Gilmore <tgilly93@gmail.com>
2025-03-24 10:36:00 -0400

    TG align with responsive structure of bootstrap in app.jsx.

--- portfolio_app/src/App.jsx
+++ portfolio_app/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import NavBar from './components/NavBar';
 
 import './App.css'
+import Container from 'react-bootstrap/esm/Container';
 
 function App() {
   
@@ -10,7 +11,10 @@ function App() {
   return (
     <>
     <NavBar />
+    <Container className='text-center mt-5'>
       <h1>Welcome to my Portfolio!</h1>
+      <p>This is a portfolio with Light & Dark mode.</p>
+    </Container>
     </>
   )
 }
