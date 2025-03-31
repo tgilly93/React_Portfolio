c6f5cfbef4eac2d6cefcb4e28fc5132c3cf53dff
Terry Gilmore <tgilly93@gmail.com>
2025-03-31 10:13:20 -0400

    TG added Resumepage & component.

---
+++ portfolio_app/src/components/ResumeForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+
+const ResumeForm = () => {
+    const resumeUrl = "/Terry_Gilmore_SWE_Resume.pdf";
+
+    return (
+        <Container
+        fluid
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ minHeight: "100vh", paddingBottom: "2rem", display: "flex", flexDirection: "column", justifyContent: "center" }}
+        >
+            <iframe
+            src={`${resumeUrl}#toolbar=0`}
+            width="90%"
+            height="90vh"
+            title="Resume PDF"
+            style={{ border: "none", borderRadius: "8px", flexGrow: 1}}
+            ></iframe>
+
+            <Button
+            variant="primary"
+            href={resumeUrl}
+            download="Terry_Gilmore_SWE_Resume.pdf"
+            className="mt-4 mb-4"
+            >
+                Download Resume
+            </Button>
+        </Container>
+    );
+};
+
+export default ResumeForm;
--- portfolio_app/src/pages/Resume.jsx
+++ portfolio_app/src/pages/Resume.jsx
@@ -1,10 +1,14 @@
 import Container from "react-bootstrap/esm/Container";
+import React from "react";
+import ResumeForm from "../components/ResumeForm";
 
 function Resume() {
   return (
     <Container className="text-center mt-5">
-      <h1>This is a Resume Page!</h1>
-      <p>This is a portfolio with Light & Dark mode.</p>
+      <h1 className="mb-4">View my Resume!</h1>
+      
+      <ResumeForm />
+      
     </Container>
   );
 }
