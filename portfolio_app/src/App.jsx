b8d61badb10e1e22a91caff24187203db2009629
Terry Gilmore <tgilly93@gmail.com>
2025-04-02 15:01:00 -0400

    TG predeployment.

--- portfolio_app/src/App.jsx
+++ portfolio_app/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import React from "react";
 import NavBar from "./components/NavBar";
