f3fd9bf9cd5ae88e0c38957e7adba4d2c9b6f9d3
Terry Gilmore <tgilly93@gmail.com>
2025-04-22 11:08:35 -0400

    TG fixing carousel responsiveness.

--- portfolio_app/src/pages/Achievements.jsx
+++ portfolio_app/src/pages/Achievements.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import PdfCarousel from "../components/PdfCarousel";
 import { Container } from "react-bootstrap";
 
-
 function Achievements() {
-    return (
-        <Container>
-            <PdfCarousel />
-        </Container>
-    );
-};
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Container className="flex-grow-1 d-flex flex-column justify-content-center">
+        <PdfCarousel />
+      </Container>
+    </div>
+  );
+}
 
 export default Achievements;
