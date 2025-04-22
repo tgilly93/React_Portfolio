1ff2d3c4687800229a8d07f37dd5d9253e78d418
Terry Gilmore <tgilly93@gmail.com>
2025-04-22 11:56:51 -0400

    TG fixing pdf responsiveness.

--- portfolio_app/src/components/PdfCarousel.jsx
+++ portfolio_app/src/components/PdfCarousel.jsx
@@ -41,39 +41,47 @@ function PdfCarousel() {
         {pdfFiles.map((pdf, i) => (
           <Carousel.Item key={i}>
             <div
-              className="pdf-wrapper"
-              style={{
-                position: "relative",
-                width: "100%",
-                paddingTop: "56.25%", 
-                overflow: "hidden",
-              }}
-              >
+              className="pdf-wrapper">
 
               <iframe
                 src={`${import.meta.env.BASE_URL}${pdf}#toolbar=0`}
                 title={`PDF ${i + 1}`}
                 allowFullScreen
-                className="pdf-iframe"
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  border: "none",
-                  height: "100%",
-                  width: "100%",
-                }}
-              />
+                className="pdf-frame"
+                />
             </div>
           </Carousel.Item>
         ))}
       </Carousel>
       <style>
         {`
+          .pdf-wrapper {
+          position: relative;
+          width: 100%;
+          padding-top: 56.25%; 
+          overflow: hidden;
+          }
+          .pdf-frame {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          border: none;
+        }
+          
           @media (max-width: 768px) {
           .pdf-wrapper iframe {
+          padding-top: 75%;
+          }
+          .pdf-frame {
+          width: 117.6%;
+          height: 117.6%;
           transform: scale(0.85);
           transform-origin: top left;
+          }
+          body {
+          overflow-x: hidden;
           }
           }
           `}
