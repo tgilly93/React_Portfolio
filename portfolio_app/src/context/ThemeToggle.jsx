32ffefb7d983d0b6c21504d6b2758e39928ac880
Terry Gilmore <tgilly93@gmail.com>
2025-04-25 12:10:58 -0400

    TG add switch icons.

--- portfolio_app/src/context/ThemeToggle.jsx
+++ portfolio_app/src/context/ThemeToggle.jsx
@@ -1,19 +1,27 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
 import Form from "react-bootstrap/Form";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 function ThemeToggle() {
-    const {theme, toggleTheme} = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
-    return (
-        <Form.Check
+  return (
+    <div className="d-flex align-items-center gap-2">
+      <FaSun color={theme === "light" ? "#f39c12" : "#ccc"} />
+      <Form.Check
         type="switch"
         id="theme-switch"
         checked={theme === "dark"}
         onChange={toggleTheme}
-        className="ms-2"
-        />
-    );
+        className="mb-0"
+        style={{
+          cursor: "pointer",
+        }}
+      />
+      <FaMoon color={theme === "dark" ? "#f1c40f" : "#ccc"} />
+    </div>
+  );
 }
 
 export default ThemeToggle;
